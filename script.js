ff1262263c39547b805d97f68195db5e9bee7f57
grasielarosa <grasielarosaa@gmail.com>
2021-07-06 11:04:07 +0200

    select level function + sweet alert

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let emoji=['🌞', '🌙', '👽', '🪐', '☄️', '✨'];
 let matrizSize;
 let itemSize;
 
-//randon emoji generator
+//random emoji generator
 const randomItems =() => {
     let emojiRandom=emoji[Math.floor(Math.random()*emoji.length)];
     return emojiRandom;
@@ -38,3 +38,74 @@ const gridGenerator = (matrizSize, itemSize) => {
     }
 }
 
+//select level 
+const selectLevel = () => {
+    swal({
+        title: 'Nuevo Juego',
+        buttons: {
+            facil: {
+                text: 'Fácil',
+                value: 'easy',
+                className: 'btn-play',
+            },
+            medio: {
+                text: 'Medio',
+                value: 'normal',
+                className: 'btn-play',
+            },
+            dificil: {
+                text: 'Difícil',
+                value: 'hard',
+                className: 'btn-play',
+            },
+        },
+    }).then((value) => {
+    switch(value) {
+
+        case 'easy':
+            matrizSize=9
+            itemSize=56
+            gridGenerator(matrizSize, itemSize);
+            break;
+            
+        case 'normal':
+            matrizSize=8
+            itemSize=63
+            gridGenerator(matrizSize, itemSize);
+            break;
+            
+        case 'hard':
+            matrizSize=7
+            itemSize=72
+            gridGenerator(matrizSize, itemSize);
+            break;
+
+        default:
+            swal({icon:'error'});
+            selectLevel();
+        }
+            
+    });
+}
+
+
+
+
+
+//rules card
+// Swal.fire({
+//     title: '¡Bienvenida!',
+//     html: '<p class="modal">En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo,ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.<br>Si se forma un grupo, esos ítems se eliminarán y ganarás puntos.¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!</p><br><span class="modal">CONTROLES</span><p class="modal">Click izquierdo: selección</p><p class="modal">Enter o Espacio: selección</p><p class="modal">Flechas o WASD: movimiento e intercambio</p>',
+//     confirmButtonText: 'JUGAR',
+//     padding:'1rem',
+//     backdrop:true,
+//     customClass: {
+//         confirmButton: 'btn-play'
+//     },
+//     showConfirmButton: true,
+//     confirmButtonColor: '#f87372',
+//     confirmButtonAriaLabel: 'Iniciar el juego',
+
+// }).then(() => {
+//     selectLevel();
+// });
